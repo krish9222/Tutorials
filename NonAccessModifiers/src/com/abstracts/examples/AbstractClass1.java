01b114d2927002d92581ea96aa836fc56173fa35
Krishna Gudala <krish@Krishnas-MacBook-Pro.local>
2020-10-28 20:08:47 -0700

    Adding loops class material

---
+++ NonAccessModifiers/src/com/abstracts/examples/AbstractClass1.java
@@ -0,0 +1,11 @@
+package com.abstracts.examples;
+
+public abstract class AbstractClass1 {
+	
+	public abstract void display(String para1);
+	
+	public void show() {
+		System.out.println("Called from show");
+	}
+	
+}
---
+++ NonAccessModifiers/src/com/abstracts/examples/TestClass.java
@@ -0,0 +1,15 @@
+package com.abstracts.examples;
+
+public class TestClass extends AbstractClass1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	@Override
+	public void display(String para1) {
+		System.out.println("");
+	}
+
+}
